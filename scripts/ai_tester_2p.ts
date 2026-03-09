b89731439dc98aef6810575445456f7fcf79c006
vincent71399 <vincent71399@gmail.com>
2026-03-09 14:37:46 +0800

    -adjust scripts

--- scripts/ai_tester_2p.ts
+++ scripts/ai_tester_2p.ts
@@ -105,4 +105,4 @@ async function runTournament(p1Type: PlayerType, p2Type: PlayerType) {
     }
 }
 
-runTournament('ai-noob', 'ai-medium').catch(console.error);
+runTournament('ai-easy', 'ai-medium').catch(console.error);
