9d0f431dd8974ed052b70675e7d99f1bae199b1c
vincent71399 <vincent71399@gmail.com>
2026-03-18 17:20:17 +0800

    -minor UI tweak for mobile

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAfKlXr7JSh-Cm4LclFqj8fM5W4jav10eA",
-    authDomain: "carcassonne-3-tiles-hand.firebaseapp.com",
-    projectId: "carcassonne-3-tiles-hand",
-    storageBucket: "carcassonne-3-tiles-hand.firebasestorage.app",
-    messagingSenderId: "905779615317",
-    appId: "1:905779615317:web:cae333012c3d6da66726dc",
-    measurementId: "G-C7NNEMV1ES"
-};
